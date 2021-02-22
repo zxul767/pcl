3583fd534fc7f0e4fdbf6aaa41088ffb711af82d
zxul767 <willebaldo.gomez@gmail.com>
2021-02-22 12:26:46 -0600

    Refactor and document `pathnames` package
    Refactoring consists of some public methods renaming.
    Documentation encompasses documentation strings for public functions.

--- src/chapter-15/packages.lisp
+++ src/chapter-15/packages.lisp
@@ -3,12 +3,12 @@
 (defpackage :dev.zxul767.pathnames
   (:use :common-lisp)
   (:export
+   :as-directory-pathname
+   :as-file-pathname
    :list-directory
    :file-exists-p
    :directory-pathname-p
    :file-pathname-p
-   :pathname-as-directory
-   :pathname-as-file
    :walk-directory
    :directory-p
    :file-p))
--- src/chapter-15/pathnames.lisp
+++ src/chapter-15/pathnames.lisp
@@ -1,11 +1,40 @@
 (in-package :dev.zxul767.pathnames)
 
 ;;
-;; public API
+;; Public API
 ;;
+(defun as-directory-pathname (name)
+  "Converts `name', if necessary, to a canonical directory pathname."
+  (let ((pathname (pathname name)))
+    (if (wild-pathname-p pathname)
+        (error "Cannot reliably convert wild pathnames"))
+    (if (directory-pathname-p pathname)
+        pathname
+        (make-pathname
+         :directory (as-directory-parts pathname)
+         :name nil
+         :type nil
+         :defaults pathname))))
+
+(defun as-file-pathname (name)
+  "Converts `name', if necessary, to a canonical file pathname."
+  (let ((pathname (pathname name)))
+    (if (wild-pathname-p pathname)
+        (error "Cannot reliably convert wild pathnames"))
+    (if (not (directory-pathname-p name))
+        pathname
+        (let* ((directory (pathname-directory pathname))
+               (name-and-type (pathname (first (last directory)))))
+          (make-pathname
+           :directory (butlast directory)
+           :name (pathname-name name-and-type)
+           :type (pathname-type name-and-type)
+           :defaults pathname)))))
+
 (defun list-directory (dirname)
-  (when (wild-pathname-p dirname)
-    (error "Can only list concrete directory names."))
+  "Returns all files and directories under `dirname', as canonical pathnames."
+  (if (wild-pathname-p dirname)
+      (error "Can only list concrete directory names."))
   (let ((wildcard (directory-wildcard dirname)))
     #+ (or sbcl cmu lispworks)
     (directory wildcard)
@@ -21,94 +50,89 @@
     (error "list-directory not implemented")))
 
 (defun file-exists-p (pathname)
+  "Returns non-nil if `pathname' represents an existing file or directory."
   #+ (or sbcl lispworks openmcl)
   (probe-file pathname)
   #+ (or allegro cmu)
-  (or (probe-file (pathname-as-directory pathname))
+  (or (probe-file (as-directory-pathname pathname))
       (probe-file pathname))
   #+clisp
   (or (ignore-errors
        (probe-file (pathname-as-file pathname)))
       (ignore-errors
-       (let ((directory-form (pathname-as-directory pathname)))
+       (let ((directory-form (as-directory-pathname pathname)))
          (when (ext:probe-directory directory-form)
            directory-form))))
   #- (or sbcl cmu lispworks openmcl allegro clisp)
   (error "file-exists-p not implemented"))
 
-(defun directory-pathname-p (p)
+(defun directory-pathname-p (pathname)
+  "Returns non-nil if `pathname' represents a directory pathname."
   (labels
       ((component-present-p (value)
          (and value (not (eql value :unspecific)))))
     (and
-     (not (component-present-p (pathname-name p)))
-     (not (component-present-p (pathname-type p)))
-     p)))
+     (not (component-present-p (pathname-name pathname)))
+     (not (component-present-p (pathname-type pathname)))
+     pathname)))
 
-(defun file-pathname-p (p)
-  (and (not (directory-pathname-p p))
-       p))
+(defun file-pathname-p (pathname)
+  "Return non-nil if `pathname' represents a file pathname"
+  (unless (directory-pathname-p pathname) pathname))
 
-(defun pathname-as-directory (name)
-  (labels
-      ((to-directory (pathname)
-         (append (or (pathname-directory pathname) (list :relative))
-                 (list (file-namestring pathname)))))
-    (let ((pathname (pathname name)))
-      (when (wild-pathname-p pathname)
-        (error "Cannot reliably convert wild pathnames."))
-      (if (directory-pathname-p pathname)
-          pathname
-          (make-pathname
-           :directory (to-directory pathname)
-           :name nil
-           :type nil
-           :defaults pathname)))))
-
-(defun pathname-as-file (name)
-  (let ((pathname (pathname name)))
-    (when (wild-pathname-p pathname)
-      (error "Cannot reliably convert wild pathnames"))
-    (if (not (directory-pathname-p name))
-        pathname
-        (let* ((directory (pathname-directory pathname))
-               (name-and-type (pathname (first (last directory)))))
-          (make-pathname
-           :directory (butlast directory)
-           :name (pathname-name name-and-type)
-           :type (pathname-type name-and-type)
-           :defaults pathname)))))
-
-;; TODO: analyze if the current implementation is flexible and sensible enough
 (defun walk-directory (dirname callback
-                       &key recursively directories (condition (constantly t)))
+                       &key
+                         recursively
+                         include-directories
+                         (condition (constantly t)))
+  "Traverse directory `dirname', invoking `callback' on each file.
+On each call to `callback' a canonical pathname is passed back.
+By default, all files (and possibly directories) are considered but `condition'
+can be used to limit which files are actually passed back via `callback'.
+By default, only top-level files are visited but `recursively' can change that.
+By default, only files are visited but `include-directories' can change that.
+
+Example: traverse the `src' directory recursively, printing each file and
+directory underneath it, excluding hidden files.
+
+(walk-directory \"~/src\" #'print
+                :recursively t
+                :include-directories t
+                :condition #'is-not-hidden-file)
+
+Notice that if `is-not-hidden-file' returns `nil' for directories (e.g., `.git'),
+then files underneath them won't be visited, even if they would pass `condition'"
   (labels
-      ((visit (name)
-         (when (funcall condition name)
-           (funcall callback name)))
-       (walk (dirname)
-         (when directories
-           (visit dirname))
-         (dolist (child (list-directory dirname))
-           (if (and recursively (directory-p child) (funcall condition child))
-               (walk child)
-               (visit child)))))
-    (walk (pathname-as-directory dirname))))
+      ((visit (pathname)
+         (if (funcall condition pathname)
+             (funcall callback pathname)))
+       (walk (dirpath)
+         (if include-directories
+             (visit dirpath))
+         (dolist (pathname (list-directory dirpath))
+           (if (and recursively (directory-p pathname) (funcall condition pathname))
+               (walk pathname)
+               (visit pathname)))))
+    (walk (as-directory-pathname dirname))))
 
-(defun file-p (path)
-  (and (file-pathname-p path) (file-exists-p path)))
+(defun file-p (name)
+  "Is `name' the name of an existing file, i.e. not a directory?"
+  (let ((truename (file-exists-p name)))
+    (and truename (file-pathname-p name))))
 
-(defun directory-p (path)
-  (not (file-p path)))
+(defun directory-p (name)
+  "Is `name' the name of an existing directory?"
+  (let ((truename (file-exists-p name)))
+    (and truename (directory-pathname-p name))))
 
 ;;
-;; implementation details
+;; Auxiliary Functions
 ;;
 (defun directory-wildcard (dirname)
   (make-pathname
    :name :wild
    :type #-clisp :wild #+clisp nil
-   :defaults (pathname-as-directory dirname)))
+   :defaults (as-directory-pathname dirname)))
 
 #+clisp
 (defun clisp-subdirectories-wildcard (wildcard)
@@ -118,7 +142,6 @@
    :type nil
    :defaults wildcard))
 
-(defun is-hidden-p (path)
-  (let ((name (or (pathname-name path)
-                  (first (last (pathname-directory path))))))
-    (and name (eql #\. (aref name 0)))))
+(defun as-directory-parts (pathname)
+  (append (or (pathname-directory pathname) (list :relative))
+          (list (file-namestring pathname))))

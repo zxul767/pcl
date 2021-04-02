ad6f4c41d1b14f70ec31f53e0d9bb88b432746d1
zxul767 <willebaldo.gomez@gmail.com>
2021-04-02 00:01:10 -0600

    Simplify and document current version of `once-only` macro

--- src/macrotools/macrotools.lisp
+++ src/macrotools/macrotools.lisp
@@ -1,5 +1,12 @@
 (in-package :dev.zxul767.macrotools)
 
+;; TODO: move to an itertools module?
+(defun zip (&rest lists)
+  (apply #'mapcar #'list lists))
+
+(defun gensyms (n)
+  (loop repeat n collect (gensym)))
+
 (defmacro with-gensyms ((&rest symbols) &body body)
   "Wrap `body' within a new lexical environment where each symbol in `symbols'
 is bound to a a \"gensym\" (a unique symbol guaranteed to not clash with any
@@ -34,12 +41,45 @@ Example:
       `(let ,(loop for name in names for hint in hints collect `(,name (gensym ,hint)))
          ,@body))))
 
-;; TODO:
-;; + rewrite to simplify and improve readability
-;; + document
 (defmacro once-only ((&rest names) &body body)
-  (let ((symbols (loop for n in names collect (gensym))))
-    `(let (,@(loop for s in symbols collect `(,s (gensym))))
-       `(let (,,@(loop for s in symbols for n in names collect ``(,,s ,,n)))
-          ,(let (,@(loop for n in names for s in symbols collect `(,n ,s)))
-             ,@body)))))
+  "Generate a macro template that, upon expansion, will rewrite `body' to guarantee that:
+1. Each value (an expression) in `names' is evaluated exactly once, and
+2. There is no need to manually introduce gensyms to acomplish (1)
+
+Example:
+(macroexpand-1 '(once-only (n) `(fibonacci ,n (1- ,n))))
+=>
+(LET ((#:G558 N) (N (GENSYM \"N\")))
+  `(LET (,`(,N ,#:G558))
+     ,`(FIBONACCI ,N (1- ,N))))
+
+When used within a macro (as it is intended):
+(defmacro test (n)
+  (evaluated-once (n)
+    `(fibonacci ,n (1- ,n))))
+
+the final expansion would be:
+(macroexpand-1 '(fibonacci (+ 1 2)))
+=>
+(LET ((#:N557 (+ 1 2)))
+   (FIBONACCI #:N557 (1- #:N557)))
+"
+  (let ((names-symbols (zip names (gensyms (length names)))))
+    `(let (,@(loop for (n s) in names-symbols collect `(,s ,n))
+           ,@(loop for n in names collect `(,n (gensym ,(string n)))))
+       `(let (,,@(loop for (n s) in names-symbols collect ``(,,n ,,s)))
+          ,,@body))))
+
+;; this is the original definition in the book (minus some minor modifications
+;; which don't modify the basic shape):
+;;
+;; (let ((names-gensyms (zip names (gensyms (length names)))))
+;;   `(let (,@(loop for (n g) in names-gensyms collect `(,g (gensym ,(string n)))))
+;;      `(let (,,@(loop for (n g) in names-gensyms collect ``(,,g ,,n)))
+;;         ,(let (,@(loop for (n g) in names-gensyms collect `(,n ,g)))
+;;            ,@body))))
+;;
+;; the version above is an alternative--and IMO simpler--definition, as it avoids
+;; one nesting level by taking advantage of the fact that (let ((n <expr>) (g n)) ...)
+;; is a perfectly valid way to use the current value of `n' while rebinding it to
+;; another value in "parallel"

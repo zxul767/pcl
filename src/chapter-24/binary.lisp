9a553f15c2027ca0510119659bf61ebcb4b2ceef
zxul767 <willebaldo.gomez@gmail.com>
2021-04-06 15:02:38 -0500

    Refactor `binary.lisp`
    + Improve naming for various functions. In particular, improve the naming and
    comments to provide context for the code that supports the processing stack of
    binary objects.
    
    + Rewrite some helper functions to shorten them while retaining (and sometimes)
    improving readability

--- src/chapter-24/binary.lisp
+++ src/chapter-24/binary.lisp
@@ -1,11 +1,17 @@
 (in-package :dev.zxul767.binary)
 
 (defconstant +null+ (code-char 0))
-(defvar *in-progress-objects* nil)
+
 
 ;; -----------------------------------------------------------------------------
 ;; Interfaces
 ;; -----------------------------------------------------------------------------
+(defgeneric read-value (type stream &key)
+  (:documentation "Read a value of the given type from the stream."))
+
+(defgeneric write-value (type stream value &key)
+  (:documentation "Write a value as the given type to the stream."))
+
 (defgeneric read-object (object stream)
   (:method-combination progn :most-specific-last)
   (:documentation "Fill in the slots of object from stream."))
@@ -14,30 +20,36 @@
   (:method-combination progn :most-specific-last)
   (:documentation "Write out the slots of object from stream."))
 
-(defgeneric read-value (type stream &key)
-  (:documentation "Read a value of the given type from the stream."))
-
-(defgeneric write-value (type stream value &key)
-  (:documentation "Write a value as the given type to the stream."))
-
 ;; -----------------------------------------------------------------------------
-;; Current Object Stack Support
+;; Binary Objects Processing Stack Support
+;;
+;; When processing binary objects, sometimes the parsing of a nested object
+;; depends on information found in a parent (or more generally an ancestor)
+;; object. The solution chosen here involves the creation of a stack of objects
+;; currently being processed (the "processing stack") implemented with a dynamic
+;; variable (*binary-objects-processing-stack*)
 ;; -----------------------------------------------------------------------------
+(defvar *binary-objects-processing-stack* nil)
+
 (defmethod read-object :around (object stream)
   (declare (ignore stream))
-  (let ((*in-progress-objects* (cons object *in-progress-objects*)))
+  (let ((*binary-objects-processing-stack*
+          (cons object *binary-objects-processing-stack*)))
     (call-next-method)))
 
 (defmethod write-object :around (object stream)
   (declare (ignore stream))
-  (let ((*in-progress-objects* (cons object *in-progress-objects*)))
+  (let ((*binary-objects-processing-stack*
+          (cons object *binary-objects-processing-stack*)))
     (call-next-method)))
 
-(defun current-binary-object ()
-  (first *in-progress-objects*))
+(defun first-in-processing-stack ()
+  (first *binary-objects-processing-stack*))
 
-(defun parent-of-type (type)
-  (find-if #'(lambda (x) (typep x type)) *in-progress-objects*))
+(defun first-ancestor-in-processing-stack-of-type (type)
+  "Return the first object of type `type' (excluding the first one in the processing
+stack) currently being read/written."
+  (find-if #'(lambda (x) (typep x type)) (rest *binary-objects-processing-stack*)))
 
 ;; -----------------------------------------------------------------------------
 ;; Default Implementations
@@ -52,12 +64,23 @@
   (write-object value stream))
 
 ;; -----------------------------------------------------------------------------
-;; General Helper Functions
+;; General Helper Functions & Macros
 ;; -----------------------------------------------------------------------------
 (defun as-keyword (symbol) (intern (string symbol) :keyword))
 
-(defun normalize-slot-spec (spec)
-  (list (first spec) (ensure-list (second spec))))
+(defun assert-all (predicate sequence)
+  (dolist (item sequence)
+    (assert (funcall predicate item))))
+
+;; (id (iso-8859-1-string :length 3)) => (id (iso-8859-1-string :length 3))
+;; (size u3))                         => (size (u3))
+(defun normalize-slot (slot)
+  (list (first slot) (ensure-list (second slot))))
+
+(defmacro with-slot-parts ((name type args) slot &body body)
+  (assert-all #'symbolp (list name type args))
+  `(destructuring-bind (,name (,type &rest ,args)) (normalize-slot ,slot)
+     ,@body))
 
 (defun ensure-list (x)
   (if (listp x) x (list x)))
@@ -89,20 +112,18 @@
        ,@read-method
 
        (defmethod write-object progn ((,object ,name) ,stream)
-         (with-slots ,(new-class-all-slots slots superclasses) ,object
-           ,@(generate-write-slots-statements slots stream))))))
+         (with-slots ,(all-combined-slots slots superclasses) ,object
+           ,@(generate-write-slot-expressions slots stream))))))
 
 (defun generate-defclass-slots (slots)
-  (flet ((slot->defclass-slot (spec)
-           (let ((name (first spec)))
-             `(,name :initarg ,(as-keyword name) :accessor ,name))))
-    (mapcar #'slot->defclass-slot slots)))
+  (loop for (name _) in slots
+        collect `(,name :initarg ,(as-keyword name) :accessor ,name)))
 
-(defun generate-write-slots-statements (slots stream)
-  (flet ((slot->write-value-statement (spec stream)
-           (destructuring-bind (name (type &rest args)) (normalize-slot-spec spec)
+(defun generate-write-slot-expressions (slots stream)
+  (flet ((slot->write-value-statement (slot)
+           (with-slot-parts (name type args) slot
              `(write-value ',type ,stream ,name ,@args))))
-    (mapcar #'(lambda (s) (slot->write-value-statement s stream)) slots)))
+    (mapcar #'slot->write-value-statement slots)))
 
 (defun direct-slots (class-symbol)
   (copy-list (get class-symbol 'slots)))
@@ -115,11 +136,11 @@
 (defun all-slots (class-symbol)
   (nconc (direct-slots class-symbol) (inherited-slots class-symbol)))
 
-;; this function is necessary because calling (all-slots <class-X>) when we're
-;; compiling <class-X> will result in an empty list since the symbol <class-X>
-;; has not yet been processed and thus has no slots or superclasses information
-;; (it will after the expansion for <class-X> is compiled/loaded at top-level)
-(defun new-class-all-slots (slots superclasses)
+;; This function is necessary because when we're macro-expanding calls to
+;; `define-binary-class', any call to (all-slots <class-symbol>) would result
+;; in an empty list, since the code to attach `slots' and `superclasses' to
+;; <class-symbol> is yet to be run after the macro expansion.
+(defun all-combined-slots (slots superclasses)
   (nconc (mapcan #'all-slots superclasses) (mapcar #'first slots)))
 
 ;; -----------------------------------------------------------------------------
@@ -130,23 +151,24 @@
 ;;
 ;; (define-binary-class generic-frame (frame)
 ;;   (data (raw-bytes :bytes size)))
+;; -----------------------------------------------------------------------------
 (defmacro define-binary-class (name (&rest superclasses) &body slots)
   (with-gensyms (object stream)
     `(define-generic-binary-class ,name ,superclasses ,slots
        (defmethod read-object progn ((,object ,name) ,stream)
-         (with-slots ,(new-class-all-slots slots superclasses) ,object
-           ,@(generate-read-slots-statements slots stream))))))
+         (with-slots ,(all-combined-slots slots superclasses) ,object
+           ,@(generate-read-slot-expressions slots stream))))))
 
-(defun generate-read-slots-statements (slots stream)
-  (flet ((slot->read-value-expression (spec stream)
-           (destructuring-bind (name (type &rest args)) (normalize-slot-spec spec)
+(defun generate-read-slot-expressions (slots stream)
+  (flet ((slot->read-value-expression (slot)
+           (with-slot-parts (name type args) slot
              `(setf ,name (read-value ',type ,stream ,@args)))))
-    (mapcar #'(lambda (s) (slot->read-value-expression s stream)) slots)))
+    (mapcar #'slot->read-value-expression slots)))
 
 ;; -----------------------------------------------------------------------------
 ;; Usage:
 ;; (define-tagged-binary-class id3-frame ()
-;;     ((:class-name-finder (find-frame-class id)))
+;;     ((:class-finder (find-frame-class id)))
 ;;   (id (iso-8859-1-string :length 3))
 ;;   (size u3))
 ;;
@@ -154,32 +176,32 @@
 ;; fields (with a mandatory ID field) but whose remaining structure depends on
 ;; the value of the ID found, so a mechanism to build the correct binary class
 ;; and dispatch the reading of its corresponding fields is required, hence the
-;; :class-name-finder argument.
+;; :class-finder argument.
+;; -----------------------------------------------------------------------------
 (defmacro define-tagged-binary-class (name (&rest superclasses) (&rest options) &body slots)
-  (let-guard (class-name-finder (second (assoc :class-name-finder options)))
-             (error ":class-name-finder option is mandatory!")
+  (let-guard (class-finder (second (assoc :class-finder options)))
+             (error ":class-finder option is mandatory!")
     (with-gensyms (type object stream)
       `(define-generic-binary-class ,name ,superclasses ,slots
          (defmethod read-value ((,type (eql ',name)) ,stream &key)
-           (let* ,(generate-slot-read-bindings slots stream)
-             (let ((,object (make-instance ,class-name-finder ,@(generate-slot-keywords slots))))
+           (let* ,(generate-read-slot-bindings slots stream)
+             (let ((,object (make-instance ,class-finder ,@(generate-slot-keywords slots))))
                (read-object ,object ,stream)
                ,object)))))))
 
-(defun generate-slot-read-bindings (slots stream)
-  (flet ((slot->binding (spec stream)
-           (destructuring-bind (name (type &rest args)) (normalize-slot-spec spec)
+(defun generate-read-slot-bindings (slots stream)
+  (flet ((slot->binding (slot)
+           (with-slot-parts (name type args) slot
              `(,name (read-value ',type ,stream ,@args)))))
-    (mapcar #'(lambda (s) (slot->binding s stream)) slots)))
+    (mapcar #'slot->binding slots)))
 
 (defun generate-slot-keywords (slots)
-  (flet ((slot->keyword-arg (spec)
-           (let ((name (first spec)))
-             `(,(as-keyword name) ,name))))
-    (mapcan #'slot->keyword-arg slots)))
+  (loop for (name _) in slots append `(,(as-keyword name) ,name)))
 
 ;; -----------------------------------------------------------------------------
 ;; Usage:
+;; (define-binary-type u1 () (unsigned-integer :bytes 1))
+;;
 ;; (define-binary-type iso-8859-1-string (length)
 ;;   (:reader (in)
 ;;            (let ((string (make-string length)))
@@ -189,8 +211,7 @@
 ;;   (:writer (out string)
 ;;            (dotimes (i length)
 ;;              (write-byte (char-code (char string i)) out))))
-;;
-;; (define-binary-type u1 () (unsigned-integer :bytes 1))
+;; -----------------------------------------------------------------------------
 (defmacro define-binary-type (name (&rest args) &body spec)
   (ecase (length spec)
     (1

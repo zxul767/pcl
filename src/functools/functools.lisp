e8c3843185e1686eac2f94910fac8852026cabd7
Willebaldo Gómez <willebaldo.gomez@gmail.com>
2022-10-13 14:07:10 -0500

    Add documentation string for a helper function

--- src/functools/functools.lisp
+++ src/functools/functools.lisp
@@ -7,5 +7,6 @@ All functions are assumed to have the same signature"
        (and ,@(loop for fn in functions collect `(apply #',fn args)))))
 
 (defun negate (function)
+  "Generate a function that is the logical opposite of `function'"
   (lambda (&rest args)
     (not (apply function args))))

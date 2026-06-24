49d341874543f7c6ee0d3622cb89870ccaf1e668
Willebaldo Gomez <willebaldo.gomez@gmail.com>
2026-06-24 00:31:10 -0600

    fix: CI fails due to "fiveam" not loaded for tests

--- src/check.lisp
+++ src/check.lisp
@@ -31,10 +31,17 @@
     :test #'string=)
    #'string<))
 
-(defun run-system-test (system-name)
+(defun run-system-test (system-name &key verbose)
   (handler-case
       (progn
         (format t "~&Testing ASDF system ~a...~%" system-name)
+
+        ;; load the tests system via quicklisp so we can fetch any required
+        ;; third-party dependencies first (e.g., "fiveam")
+        (let ((system-tests-name (format nil "~a/tests" system-name)))
+          (if (asdf:find-system system-tests-name nil)
+              (ql:quickload system-tests-name :silent (not verbose) :verbose verbose)))
+
         (asdf:test-system system-name)
         nil)
     (error (condition)
@@ -42,9 +49,9 @@
               system-name condition)
       (cons system-name condition))))
 
-(defun run-system-tests (system-names)
+(defun run-system-tests (system-names &key verbose)
   (loop for name in system-names
-        for failure = (run-system-test name)
+        for failure = (run-system-test name :verbose verbose)
         when failure collect failure))
 
 (defun verbose-checks-p ()
@@ -86,14 +93,14 @@
                 (invoke-with-warning-capture stream)))
         (report-suppressed-warnings warning-count warnings-file)))))
 
-(defun project-check-status (source-directory verbose)
+(defun project-check-status (source-directory &key verbose)
   (handler-case
-      (progn
+      (asdf/session:with-asdf-session (:override t)
         ;; Compile and load the top-level application and all dependent subsystems.
         (ql:quickload *main-system* :silent (not verbose) :verbose verbose)
         ;; Run all subsystems' tests.
         (let ((failures
-                (run-system-tests (local-system-names source-directory))))
+                (run-system-tests (local-system-names source-directory) :verbose verbose)))
           (when failures
             (error "~d ASDF system~:p failed project checks."
                    (length failures))))
@@ -107,7 +114,7 @@
   (let* ((verbose (verbose-checks-p)))
     (uiop:quit
      (run-function
-      (lambda () (project-check-status (get-source-directory) verbose))
+      (lambda () (project-check-status (get-source-directory) :verbose verbose))
       :verbose verbose))))
 
 (run-project-checks)

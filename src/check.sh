73d4c74a38f76ec20675cf6eb76e73f4c1def37c
zxul767 <willebaldo.gomez@gmail.com>
2026-07-01 21:45:15 -0600

    fix: `check.sh` is failing in CI
    also, `ros` was not using `LISP` when running tests in CI

--- src/check.sh
+++ src/check.sh
@@ -11,7 +11,14 @@ if [[ -n "${LISP:-}" ]]; then
 fi
 
 # Run Roswell with Quicklisp enabled (-Q), load the script (-l), then quit (-q).
-exec ros "${ros_args[@]}" -Q \
-    -e "(pushnew :run-main *features*)" \
-    -l "$script_dir/check.lisp" \
-    -q
+if ((${#ros_args[@]})); then
+    exec ros "${ros_args[@]}" -Q \
+        -e "(pushnew :run-main *features*)" \
+        -l "$script_dir/check.lisp" \
+        -q
+else
+    exec ros -Q \
+        -e "(pushnew :run-main *features*)" \
+        -l "$script_dir/check.lisp" \
+        -q
+fi
